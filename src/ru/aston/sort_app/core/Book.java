8fbf6a8728bc651daba13982fa1ced61974ca686
Alex <alex@example.com>
2024-11-23 16:07:30 +0300

    Реализован Book

--- src/ru/aston/sort_app/core/Book.java
+++ src/ru/aston/sort_app/core/Book.java
@@ -1,4 +1,79 @@
 package ru.aston.sort_app.core;
 
+import java.util.Objects;
+
 public class Book {
+    private String author;
+    private String title;
+    private int pageCount;
+
+    private Book(String author, String title, int pageCount) {
+        this.author = author;
+        this.title = title;
+        this.pageCount = pageCount;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public int getPageCount() {
+        return pageCount;
+    }
+
+    public static class Builder {
+        private String author;
+        private String title;
+        private int pageCount;
+
+        public Builder setAuthor(String author) {
+            this.author = author;
+            return this;
+        }
+
+        public Builder setTitle(String title) {
+            this.title = title;
+            return this;
+        }
+
+        public Builder setPageCount(int pageCount) {
+            this.pageCount = pageCount;
+            return this;
+        }
+
+        public Book build() {
+            if (author == null || title == null || pageCount <= 0) {
+                throw new IllegalArgumentException("Некорректные данные для книги.");
+            }
+            return new Book(author, title, pageCount);
+        }
+    }
+
+    @Override
+    public String toString() {
+        return "Book{" +
+                "author='" + author + '\'' +
+                ", title='" + title + '\'' +
+                ", pageCount=" + pageCount +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Book book = (Book) o;
+        return pageCount == book.pageCount &&
+                Objects.equals(author, book.author) &&
+                Objects.equals(title, book.title);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(author, title, pageCount);
+    }
 }
