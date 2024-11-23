cac74ada82142ac19cc1f4f1232bdb98f4d42f8c
ReducetoLover <80139283+ReducetoLover@users.noreply.github.com>
2024-11-23 18:10:06 +0700

    enum для выбора в меню

--- src/ru/aston/sort_app/core/UserInputChoice.java
+++ src/ru/aston/sort_app/core/UserInputChoice.java
@@ -1,7 +1,50 @@
 package ru.aston.sort_app.core;
 
 public enum UserInputChoice {
-    RANDOM_GENERATION,
+    ADD_DATA,
     USER_GENERATED,
-    FILE
+    CAR_MANUAL,
+    BOOK_MANUAL,
+    ROOTCROP_MANUAL,
+
+    RANDOM_GENERATION,
+    CAR_RANDOM,
+    BOOK_RANDOM,
+    ROOTCROP_RANDOM,
+
+    FILE_GENERATED,
+    CAR_FILE,
+    BOOK_FILE,
+    ROOTCROP_FILE,
+
+
+    CAR_POWER,
+    CAR_MODEL,
+    CAR_YEAR,
+
+    BOOK_AUTHOR,
+    BOOK_TITLE,
+    BOOK_PAGE_COUNT,
+
+    ROOTCROP_TYPE,
+    ROOTCROP_WEIGHT,
+    ROOTCROP_COLOR,
+
+    STOP_SORT,
+    SEARCH,
+    CAR_SEARCH,
+    BOOK_SEARCH,
+    ROOTCROP_SEARCH,
+
+    SORT,
+    CAR_SORT,
+    BOOK_SORT,
+    ROOTCROP_SORT,
+
+    BACK_MENU,
+    BACK,
+
+    SAVE_DATA,
+    EXIT
+
 }
